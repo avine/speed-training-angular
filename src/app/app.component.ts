87392fd08531e2ad076e772126ce80b7aee696ca
Stéphane Francel <stephane.francel@gmail.com>
2023-11-16 17:39:17 +0100

    forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'speed-training-angular';
+
+  formGroup = new FormGroup({
+    email: new FormControl('', { validators: [Validators.required, Validators.email], nonNullable: true }),
+    message: new FormControl('', [Validators.minLength(5)]),
+  });
+
+  onSubmit() {
+    console.log(this.formGroup.value);
+  }
 }
